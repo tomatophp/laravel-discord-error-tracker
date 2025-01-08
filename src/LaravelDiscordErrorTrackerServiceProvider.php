403c6be586c9f8b4eb26789163d9d720b0220ec4
3x1io <3x1io@users.noreply.github.com>
2025-01-08 03:37:12 +0000

    Format Code

--- src/LaravelDiscordErrorTrackerServiceProvider.php
+++ src/LaravelDiscordErrorTrackerServiceProvider.php
@@ -9,11 +9,11 @@ class LaravelDiscordErrorTrackerServiceProvider extends ServiceProvider
     public function register(): void
     {
         // Register Config file
-        $this->mergeConfigFrom(__DIR__.'/../config/laravel-discord-error-tracker.php', 'laravel-discord-error-tracker');
+        $this->mergeConfigFrom(__DIR__ . '/../config/laravel-discord-error-tracker.php', 'laravel-discord-error-tracker');
 
         // Publish Config
         $this->publishes([
-            __DIR__.'/../config/laravel-discord-error-tracker.php' => config_path('laravel-discord-error-tracker.php'),
+            __DIR__ . '/../config/laravel-discord-error-tracker.php' => config_path('laravel-discord-error-tracker.php'),
         ], 'laravel-discord-error-tracker-config');
     }
 
